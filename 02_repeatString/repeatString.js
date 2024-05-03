b96442fd43c9630a186d20914623dc19360f5e32
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-03 15:27:48 -0400

    Handle negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,11 @@
 const repeatString = function(string, num) {
     let newString = "";
-    for (let i = 0; i < num; i++) {
-        newString += string;
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            newString += string;
+        }
+    } else {
+        return "ERROR";
     }
     return newString;
 };
