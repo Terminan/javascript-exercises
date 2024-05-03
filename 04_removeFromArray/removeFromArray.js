a01ede439fe105fb6fc1971dbf552e7266547dc4
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-03 16:00:01 -0400

    Create function to remove values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removals) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let checkToKeep = true;
+        for (let j = 0; j < removals.length; j++) {
+            checkToKeep = true;
+            if (array[i] === removals[j]) {
+                checkToKeep = false;
+            }
+        }
+        if (checkToKeep) {
+            newArray.push(array[i]);
+        }
+    }
 };
 
 // Do not edit below this line
