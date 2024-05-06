446139a81da9430b3c73147c773f7e9624617bbe
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-06 12:03:55 -0400

    Solve test for sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(int1, int2) {
     if (typeof int1 === "number" && typeof int2 === "number") {
-        if (!int1 < 0 || !int2 < 0) {
+        if (int1 > 0 && int2 > 0) {
             let array = [];
             let sum = 0;
             if (int1 < int2) {
