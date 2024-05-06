8a63f2f1afe67e941470aa1c05b734d58cac69c5
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-06 12:01:59 -0400

    Handle negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (typeof int1 === "number" && typeof int2 === "number") {
+        if (!int1 < 0 || !int2 < 0) {
+            let array = [];
+            let sum = 0;
+            if (int1 < int2) {
+                while (int1 <= int2) {
+                    sum += int1;
+                    int1++;
+                }
+            } else {
+                while (int2 <= int1) {
+                    sum += int2;
+                    int2++;
+                }
+            }
+            return sum;
+        } else {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
