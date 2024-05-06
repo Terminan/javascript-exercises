182fe5735dd290e4d4159fc12ef5536b3969d7dc
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-06 12:23:32 -0400

    Correct reversed functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(number) {
-  return Math.round((number * 9 / 5 + 32) * 10) / 10;
+  return Math.round(((number - 32) * 5 / 9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(number) {
-  return Math.round(((number - 32) * 5 / 9) * 10) / 10;
+  return Math.round((number * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
