89cdd8782dbe1de5242b281e92e60a96a64d4b1d
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-13 15:55:02 -0400

    Solve test for findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort((a, b) => {
+        if (typeof a.yearOfDeath !== "number") {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (typeof b.yearOfDeath !== "number") {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return array[0];
 };
 
 // Do not edit below this line
