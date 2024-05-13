57421cde24a6f1025c051ea931b9ac71b0f27bab
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-13 15:07:22 -0400

    Solve test for plaindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const simplified = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversed = Array.from(simplified).reverse().join("");
+    return simplified === reversed;
 };
 
 // Do not edit below this line
