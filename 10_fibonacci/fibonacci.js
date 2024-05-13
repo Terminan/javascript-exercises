de34f91d9304509d5f1775f3f7a472e8d199ec77
Terminan <167124980+Terminan@users.noreply.github.com>
2024-05-13 15:37:20 -0400

    Solve test for fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num !== "number") {
+        num = parseInt(num);
+    }
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    let array = [1, 1];
+    while (array.length < num) {
+        array.push(array[array.length - 1] + array[array.length - 2]);
+    }
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
